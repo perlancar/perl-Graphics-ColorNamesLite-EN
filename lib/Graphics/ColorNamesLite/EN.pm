2e5e2f7340d2c089e2dd28de5e3ec84b78e9f6fb
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-30 15:24:39 +0700

    Initial commit

---
+++ lib/Graphics/ColorNamesLite/EN.pm
@@ -0,0 +1,21 @@
+package Graphics::ColorNamesLite::EN;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# note: SVG=CSS, WWW=CSS. GrayScale returns coderef.
+# CODE: no strict 'refs'; use Data::Dump; my $t = {}; for my $m (qw/CSS Crayola EmergyC HTML IE Mozilla Netscape VACCC Werner Windows X/) { my $mod = "Graphics::ColorNames::$m"; (my $modpm = "$mod.pm") =~ s!::!/!g; require $modpm; my $t2 = &{"$mod\::NamesRgbTable"}(); die "$m" unless ref $t2 eq "HASH"; for (keys %$t2) { $t->{$_} = sprintf "%06x", $t2->{$_} unless defined $t->{$_} } } print 'our $NAMES_RGB_TABLE = '; dd $t; print ";";
+
+1;
+# ABSTRACT: Lots of English color names and equivalent RGB values (lite version)
+
+=head1 DESCRIPTION
+
+This is a merge of various English Graphics::ColorNames::* modules.
+
+
+=head1 SEE ALSO
+
+C<Graphics::ColorNames::*>
