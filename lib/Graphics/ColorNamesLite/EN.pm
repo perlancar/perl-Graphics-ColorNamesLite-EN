225934fe483c30380cf5c5341a862cdda2fc3309
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-11-30 15:26:01 +0700

    Add Pantone & PantoneReport

--- lib/Graphics/ColorNamesLite/EN.pm
+++ lib/Graphics/ColorNamesLite/EN.pm
@@ -6,7 +6,7 @@ package Graphics::ColorNamesLite::EN;
 # VERSION
 
 # note: SVG=CSS, WWW=CSS. GrayScale returns coderef.
-# CODE: no strict 'refs'; use Data::Dump; my $t = {}; for my $m (qw/CSS Crayola EmergyC HTML IE Mozilla Netscape VACCC Werner Windows X/) { my $mod = "Graphics::ColorNames::$m"; (my $modpm = "$mod.pm") =~ s!::!/!g; require $modpm; my $t2 = &{"$mod\::NamesRgbTable"}(); die "$m" unless ref $t2 eq "HASH"; for (keys %$t2) { $t->{$_} = sprintf "%06x", $t2->{$_} unless defined $t->{$_} } } print 'our $NAMES_RGB_TABLE = '; dd $t; print ";";
+# CODE: no strict 'refs'; use Data::Dump; my $t = {}; for my $m (qw/CSS Crayola EmergyC HTML IE Mozilla Netscape Pantone PantoneReport VACCC Werner Windows X/) { my $mod = "Graphics::ColorNames::$m"; (my $modpm = "$mod.pm") =~ s!::!/!g; require $modpm; my $t2 = &{"$mod\::NamesRgbTable"}(); die "$m" unless ref $t2 eq "HASH"; for (keys %$t2) { $t->{$_} = sprintf "%06x", $t2->{$_} unless defined $t->{$_} } } print 'our $NAMES_RGB_TABLE = '; dd $t; print ";";
 
 1;
 # ABSTRACT: Lots of English color names and equivalent RGB values (lite version)
